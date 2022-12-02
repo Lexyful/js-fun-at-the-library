7786e1fb5acdb72ad62b768f41dfda5a3bd282f6
Alexys Jordan <alexyskjordan@gmail.com>
2022-12-01 21:05:31 -0700

    add booktest.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`
 }
+
+function buildMainCharacter(name, age, pronouns) {
+    var mainCharacter = {
+      name,
+      age,
+      pronouns
+    }
+
+    return mainCharacter
+   }
+ function saveReview(newReviews, currentReviews) {
+  if(currentReviews.includes(newReviews)){
+
+
+  } else 
+  return currentReviews.push(newReviews)
+  }
+
+  function calculatePageCount(title){
+    return title.length * 20 
+  }
+
+  function writeBook(title, mainCharacter, genre) {
+    var book = {
+      title: title,
+      mainCharacter: mainCharacter,
+      pageCount: calculatePageCount(title),
+      genre: genre
+   }
+
+   return book
+  }
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
+   buildMainCharacter,
+   saveReview,
+   calculatePageCount,
+   writeBook,
   // editBook
 }
