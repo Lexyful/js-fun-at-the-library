ce7d79f978f0306aa09107b481ba945adf9a053e
Alexys Jordan <alexyskjordan@gmail.com>
2022-12-03 16:46:05 -0700

    add shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,31 @@
 
+function shelfBook(books, shelf) {
+  if(shelf.length <3) {
+  return shelf.unshift(books)
+  }
+}
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) { 
+    if(shelf[i].title === book) {
+    return shelf.splice(i, 1)
+  }
+}
+}
+
+function listTitles(shelf) {
+  var titles = []
+  for(var i = 0; i < shelf.length; i++) {
+     titles.push(shelf[i].title) 
+
+  }
+return titles.join(', ') 
+
+}
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+   shelfBook,
+   unshelfBook,
+   listTitles,
   // searchShelf
 };
