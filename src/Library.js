575cc2e927f042acd659499283319925f6f6e6f6
Alexys Jordan <alexyskjordan@gmail.com>
2022-12-04 15:04:18 -0700

    add Library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,31 @@
 
+function createLibrary(libraryName) {
+    return {
+      name : libraryName, 
+      shelves: {fantasy: [], fiction:[], nonFiction: []}
+    }
+
+  }
+function addBook(libraryName, bookName){
+if(bookName.genre === 'fantasy') {
+  return libraryName.shelves.fantasy.push(bookName)
+
+   } else if(bookName.genre== 'nonFiction')
+   return libraryName.shelves.nonFiction.push(bookName)
+
+  }
+
+function checkoutBook(libraryName, bookName, shelves) {
+  for (var i = 0; i < shelf.length; i++) { 
+    if(shelves[0].bookName) {
+    return libraryName.shelves[i].splice(i, 1)
+  }
+}
+}
+
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+   createLibrary,
+   addBook,
+   checkoutBook
 };
